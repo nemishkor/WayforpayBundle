37a17bd1a9380595a1baeda879353db4a176b903
Vitalii Koreniev <v.korenev@otpusk.com>
2020-03-18 22:02:26 +0200

    created empty service class, moved service file

---
+++ src/nemishkor/WayForPay/Services/WayforpayService.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace nemishkor\WayForPay\Services;
+
+
+class WayforpayService {
+
+    public function purchase(): string {
+        return 'yes';
+    }
+
+}
