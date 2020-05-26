484d2d4a95933865a700e45386ff39c7b9cb087c
Vitalii Koreniev <v.korenev@otpusk.com>
2020-05-26 23:19:20 +0300

    replace implementation with interface: copied comments and updated phpDoc

--- src/ObjectValues/Order/OrderInterface.php
+++ src/ObjectValues/Order/OrderInterface.php
@@ -15,34 +15,116 @@ use DateTime;
 
 interface OrderInterface {
 
+    /**
+     * Unique number of the order in merchant’s system
+     *
+     * @return int
+     * @var int
+     */
     public function getReference(): int;
 
+    /**
+     * Date of order placing
+     *
+     * @return DateTime
+     * @var DateTime
+     */
     public function getDate(): DateTime;
 
+    /**
+     * Amount of order
+     *
+     * @return float
+     * @var float
+     */
     public function getAmount(): float;
 
+    /**
+     * Currency of order UAH
+     *
+     * @return string
+     * @var string
+     */
     public function getCurrency(): string;
 
+    /**
+     * Alternative amount of order
+     *
+     * @return float|null
+     * @var float|null
+     */
     public function getAlternativeAmount(): ?float;
 
+    /**
+     * Alternative currency of order (USD, EUR, RUR)
+     *
+     * @return string|null
+     * @var string|null
+     */
     public function getAlternativeCurrency(): ?string;
 
+    /**
+     * Period of validity of funds blocking in seconds. By default: 1 728 000 (20 days).
+     *
+     * @return DateInterval
+     * @var DateInterval
+     */
     public function getHoldTimeout(): DateInterval;
 
+    /**
+     * Sets the interval within which the order can be paid for
+     *
+     * @return DateInterval|null
+     * @var DateInterval|null
+     */
     public function getOrderTimeout(): ?DateInterval;
 
+    /**
+     * Card token for recurring withdrawals
+     *
+     * @return string|null
+     * @var string|null
+     */
     public function getRecToken(): ?string;
 
+    /**
+     * Array with the products of order
+     *
+     * @return array
+     * @var Product[]
+     */
     public function getProducts(): array;
 
+    /**
+     * Unique identifier in merchant’s system (login, email and etc.)
+     *
+     * @return string|null
+     * @var string|null
+     */
     public function getClientAccountId(): ?string;
 
+    /**
+     * @return string|null
+     * @var string|null
+     */
     public function getSocialUri(): ?string;
 
+    /**
+     * @return ContactInterface|null
+     * @var ContactInterface|null
+     */
     public function getClient(): ?ContactInterface;
 
+    /**
+     * @return ContactInterface|null
+     * @var Contact|null
+     */
     public function getDelivery(): ?ContactInterface;
 
+    /**
+     * @return Avia|null
+     * @var Avia|null
+     */
     public function getAvia(): ?Avia;
 
 }
